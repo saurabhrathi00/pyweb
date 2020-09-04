113785a7b8df032ae70ba20a86c039e86a7dfc8f
sarathi <sarathi@expediagroup.com>
2020-09-04 17:06:22 +0530

    GET and PUT created + new files added

--- chat_project/mysite/chat/urls.py
+++ chat_project/mysite/chat/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/group',views.createGroupForUser),
+    path('users/<str:username>/group',views.updateGroupForUser),
 ]
